4a54d919e3e1f429c1b5e55b99e351481d7c8c94
FlorianKuehne <campino1002000+github@gmail.com>
2016-02-26 14:04:31 +0100

    using functions & added check for connectivity
    ~ did some clean up and moved stuff into separate functions
    + added connectivity check on start, that way the script should wait until online connectivity is established 
       before trying to look up for the newest version. If non connection is established it waits 2 minutes and trys again.

--- non-free-Flash-updater.sh
+++ non-free-Flash-updater.sh
@@ -14,36 +14,92 @@
 # chmod +x "filename"
 # put link to scriptfile into /etc/rc.local
 
-enableErrorOutput="y"  #y/n
+#UserVars:
+enableErrorOutput="n"  #y/n
 
-if [ $enableErrorOutput = "y" ]
-	then
+
+#Functions
+function isOnline {
+	ping -q -w 1 -c 1 8.8.8.8 > /dev/null
+	#if [[ $(echo $?) -eq 0 ]]; then
+		##echo online
+		#return 1
+	#else
+		##echo offline
+		#return 0
+	#fi
+}
+
+# return 0 if update avail. else 1
+function checkForUpdate {
+	VER_INST=$(update-flashplugin-nonfree --status | awk 'NR==1 {print $NF }' )
+	VER_RELEASED=$(update-flashplugin-nonfree --status | awk 'NR==2 {print $NF }' )
+	#echo "$VER_INST"
+	#echo "$VER_RELEASED"
+	
+	#entfernt alle Zahlen der Versionsnummer vor dem letzten Punkt
+	VER_INST_s=${VER_INST##*.}
+	VER_RELEASED_s=${VER_RELEASED##*.}
+	#echo "$VER_RELEASED_s"
+	#echo "$VER_INST_s"
+	if [[ $VER_RELEASED_s -eq $VER_INST_s ]]; then
+		echo $(date) ": No update avaiable"
+		#echo -e "nope ...\nnothing here left for us to do.. I'm out."
+		echo "nothing here left for us to do.. I'm out."
+		return 1
+		#exit 0 	#we're done here, but this way it would be ugly as hell
+	else
+		# not required happens in updatefunction
+		#echo $(date) ": Yes update avaiable"
+		return 0
+	fi
+}
+
+function update {	
+	update-flashplugin-nonfree --install
+	if [[ $(echo $?) -eq 0 ]]; then
+		echo $(date) ": update found & succesfull installed."
+		return 0
+	else
+		echo $(date) ": some thing went wrong."
+		return 1
+	fi
+}
+
+
+
+
+#Script
+if [ $enableErrorOutput = "y" ]; then 
 	grep da * 2>> ./errors.log
 fi
 
+#it's beein overwritten on every start
 exec $1 > ./output.log
 
-VER_INST=$(update-flashplugin-nonfree --status | awk 'NR==1 {print $NF }' )
-VER_RELEASED=$(update-flashplugin-nonfree --status | awk 'NR==2 {print $NF }' )
-
-#echo "$VER_INST"
-#echo "$VER_RELEASED"
-
-#entfernt alle Zahlen der Versionsnummer vor dem letzten Punkt
-VER_INST_s=${VER_INST##*.}
-VER_RELEASED_s=${VER_RELEASED##*.}
-#echo "$VER_RELEASED_s"
-#echo "$VER_INST_s"
-
-if [[ $VER_RELEASED_s -eq $VER_INST_s ]]; then
-	echo $(date) ": No update avaiable"
-else
-	update-flashplugin-nonfree --install
-	if [[ $(echo $?) -eq 0 ]]; then
-		echo $(date) ": update found & installed."
+n=0
+while :
+do
+	((n++))
+	# if machine is online 
+	if  isOnline ; then 
+		echo "online"
+		#and an update avaiable ?
+		if  checkForUpdate ; then
+			#yes, then install the fucking thing and quit right away (exit 0 | 1)
+			update
+			echo
+			exit $(echo $?)
+		else
+			# no, done. 
+			echo
+			exit 0
+		fi
 	else
-		echo $(date) ": some thing went wrong."
+		echo "offline, gonna wait for 120s ..., tryed $n times"
 	fi
-fi
+	sleep 120
+done
+	
 
-exit 0
+exit 2
